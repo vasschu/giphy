1aa6e5584142b9f3e9cd6e721fb6836bd493e376
Niki Minev <niki.minev.a24@learn.telerikacademy.com>
2020-07-24 01:27:34 +0300

    added the giphy API key and endpoints URL in constants

--- common.js
+++ common.js
@@ -1,1 +1,7 @@
 /// this is where all constants will be declared
+
+const apiKey = '&api_key=7B0fC0XIQT4lIpcETj4FoZRjvantPCul';
+const trendingEndpoint = 'api.giphy.com/v1/gifs/trending?';
+const uploadEndpoint = 'upload.giphy.com/v1/gifs?';
+const searchEndpoint = 'api.giphy.com/v1/gifs/search?';
+const queurySearchDeclaration = '&q=';
