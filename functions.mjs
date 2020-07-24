f52fadad77f641f81954b13ba1d87100068d6650
Niki Minev <niki.minev.a24@learn.telerikacademy.com>
2020-07-24 08:50:49 +0000

    deleted .mjs file

--- functions.mjs
+++
@@ -1,9 +0,0 @@
-// this is where all functions will go
-
-export const visualizeGif = (gif, container) => {
-    const $div = $(container);
-    $div.append(`
-      <span><img src="${gif}"></span>
-    `);
-};
-
