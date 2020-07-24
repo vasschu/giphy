a8556f8e9503307cc5235a62a31805ae7803e291
Niki Minev <niki.minev.a24@learn.telerikacademy.com>
2020-07-24 08:50:40 +0000

    deleted .mjs file

--- common.mjs
+++
@@ -1,7 +0,0 @@
-/// this is where all constants will be declared
-
-export const apiKey = 'api_key=7B0fC0XIQT4lIpcETj4FoZRjvantPCul';
-export const trendingEndpoint = 'http://api.giphy.com/v1/gifs/trending?';
-export const uploadEndpoint = 'http://upload.giphy.com/v1/gifs?';
-export const searchEndpoint = 'http://api.giphy.com/v1/gifs/search?';
-export const queurySearchDeclaration = '&q=';
