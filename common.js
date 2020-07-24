6a3f687df75e5b5fe2c915544e68c74b6f5e5d41
Niki Minev <niki.minev.a24@learn.telerikacademy.com>
2020-07-24 14:58:39 +0300

    added Upload nav link dynamic transition to new page

--- common.js
+++ common.js
@@ -5,3 +5,5 @@ export const trendingEndpoint = 'http://api.giphy.com/v1/gifs/trending?';
 export const uploadEndpoint = 'http://upload.giphy.com/v1/gifs?';
 export const searchEndpoint = 'http://api.giphy.com/v1/gifs/search?';
 export const queurySearchDeclaration = '&q=';
+export const $uploadGif = $('#upload');
+export const $mainGifContainer = $('.main-gif-container');
--- events.js
+++ events.js
@@ -1,1 +1,24 @@
 // this is where all events will go
+import * as common from './common.js'
+
+$(() => {
+    common.$uploadGif.click((e) => {
+      e.preventDefault();
+      common.$mainGifContainer.html(`
+          <div class="upload-gif-container">
+            <div>
+              <h2>Upload a Gif</h2>
+            </div>
+            <div>
+              <p>Uploading a gif is just as easy, as enjoying one.<p> 
+            </div>
+            <div>
+              <input type="file" id="file-upload-box"
+            </div>
+            <div>
+              <input type="submit" id="submit-upload-button" value="Upload">
+            </div>
+          </div>
+        `);
+    });
+})();
