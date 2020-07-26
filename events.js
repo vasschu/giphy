0b2f7df9c00c9f3e7366aabd54922f117cc4bca2
Niki Minev <niki.minev.a24@learn.telerikacademy.com>
2020-07-26 12:28:00 +0300

    implemented working upload endpoint

--- events.js
+++ events.js
@@ -3,22 +3,21 @@ import * as common from './common.js'
 import { getTrending } from './service.js'
 
 $(() => {
+  common.$trendingGifs.click((e) => {
+    e.preventDefault()
+    common.$mainGifsContainer.empty();
+    getTrending();
+  });
 
-    common.$trendingGifs.click((e) => {
-        e.preventDefault()
-        common.$mainGifsContainer.empty();
-        getTrending();
-    });
+  common.$leftNavbarLogo.click((e) => {
+    e.preventDefault()
+    common.$mainGifsContainer.empty();
+    getTrending();
+  });
 
-    common.$leftNavbarLogo.click((e) => {
-        e.preventDefault()
-        common.$mainGifsContainer.empty();
-        getTrending();
-    });
-
-    common.$uploadGifs.click((e) => {
-        e.preventDefault();
-        common.$mainGifsContainer.html(`
+  common.$uploadGifs.click((e) => {
+    e.preventDefault();
+    common.$mainGifsContainer.html(`
           <div class="upload-gif-container">
             <div>
               <h2>Upload a Gif</h2>
@@ -29,10 +28,26 @@ $(() => {
             <div>
               <input type="file" id="file-upload-box"
             </div>
-            <div>
+             <div>
               <input type="submit" id="submit-upload-button" value="Upload">
             </div>
           </div>
         `);
+  });
+
+  $(document).on('change', '#file-upload-box', (event) => {
+    const uploadedGif = event.target.files[0];
+    const newForm = new FormData();
+    newForm.append( 'file', uploadedGif );
+
+    $('#submit-upload-button').click(() => {
+      fetch(`${common.uploadEndpoint}${common.apiKey}`, {
+        method: 'POST',
+        body: newForm,
+      })
+          .then((res) => res.json())
+          .then((data) => console.log(data))
     });
-})();
+  });
+});
+
