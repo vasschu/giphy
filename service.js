f76cf3a13fe7a8811bd4870013939fd15b8aff04
Vassil Christov <vassil.christov@gmail.com>
2020-07-29 21:46:55 +0300

    - documentation for the service.js file

--- service.js
+++ service.js
@@ -6,6 +6,11 @@ import {
   visualizeGif
 } from './functions.js'
 
+/**
+ * Get data from Giphy using Trenidng Gifs endpoint with our api_key.
+ * @param {variable} offset Specifies the starting position of the results. Defaults to 0.
+ * @return {undefined} functions populates the main gif container with the results by calling the visualizeGif function. Function return undefined.
+ */
 export const getTrending = (offset = 0) => {
   const $body = $('.main-gif-container');
   fetch(`${common.trendingEndpoint}${common.apiKey}&offset=${offset}`)
@@ -15,9 +20,12 @@ export const getTrending = (offset = 0) => {
       res.forEach((element) => visualizeGif(element.id, element.images.fixed_height.url, $body))
     });
 };
-getTrending()
-// ---------------
 
+/**
+ * Get data from Giphy using Get Gifs by ID endpoint with our api_key. Visualize GIFs marked as favorite. Favorite gifs are stored in local storage.
+ *
+ * @return {undefined} functions populates the main gif container with the results by calling the visualizeGif function.  Function return undefined.
+ */
 export const getFavorite = () => {
   const $body = $('.favorite-gifs-container');
   fetch(`${common.getGifsByIdEndpoint}${common.apiKey}&ids=${localStorage.getItem('favorite-id')}`)
@@ -27,7 +35,11 @@ export const getFavorite = () => {
       res.forEach((element) => visualizeGif(element.id, element.images.fixed_height.url, $body));
     });
 };
-// ---------------
+/**
+ * Get data from Giphy using Get Gifs by ID endpoint with our api_key. Visualize GIFs uploaded with our api key. Uploaded gifs are stored in local storage.
+ *
+ * @return {undefined} functions populates the main gif container with the results by calling the visualizeGif function.  Function return undefined.
+ */
 export const getUploaded = () => {
   const $body = $('.uploads-container');
   fetch(`${common.getGifsByIdEndpoint}${common.apiKey}&ids=${localStorage.getItem('upload-id')}`)
@@ -38,6 +50,13 @@ export const getUploaded = () => {
     })
 };
 
+/**
+ * Get data from Giphy using search argument and the search endpoint with our api_key
+ *
+ * @param {variable} searchTerm word taken from the search input field.
+ * @param {variable} offset Specifies the starting position of the results. Defaults to 0.
+ * @return {undefined} functions populates the main gif container with the results by calling the visualizeGif function.  Function return undefined.
+ */
 export const searchGif = (searchTerm, offset = 0) => {
   fetch(`${common.searchEndpoint}${common.queurySearchDeclaration}${searchTerm}&${common.apiKey}&offset=${offset}`)
     .then((res) => res.json())
